b9e7b037f0c83134f2e4ede99dc0f3c7a41a3937
Tesfa M <setesfaalemmesele@gmail.com>
2022-03-08 16:44:07 +0300

    config chnage

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'https://system.kehaiskepe.com/';
+$config['base_url'] = 'https://sales.kehaiskepe.com/';
 
 /*
 |--------------------------------------------------------------------------
