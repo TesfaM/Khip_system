85e9d29c8232fd42259ba2ad1d3e9e21c84c6fd1
Tesfa M <setesfaalemmesele@gmail.com>
2022-03-08 16:50:20 +0300

    database config file change

--- application/config/database.php
+++ application/config/database.php
@@ -3,8 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $active_group = 'default';
 $query_builder = TRUE;
-$hostname ='127.0.0.1';
-$username ='root';
+$hostname ='localhost';
+$username ='kehaiskepecom_kehaiskepecom';
 $password ='&cn}&dKipWM1';
 $database ='kehaiskepecom_khip';	
 
